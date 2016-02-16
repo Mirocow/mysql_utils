ea01afbb3dda6b10538f65e09c1057f895bb0333
mirocow <Mirocow@users.noreply.github.com>
2016-02-16 14:51:03 +0400

    some fixes

--- restore.sh
+++ restore.sh
@@ -52,105 +52,107 @@ restore()
 	fi
 
 	for i in $(ls -1 -d $DIR/*); do
+
+		BDD=$(basename $i)
+		
+		for skip in "${DATABASES_SKIP[@]}"; do
+			if [ $BDD = $skip ]; then
+				f_log "Skip database $BDD"
+				unset BDD
+				break
+			fi								
+		done
+		
+		for select in "${DATABASES_SELECTED[@]}"; do
+			if [ $BDD != $select ]; then
+				f_log "Skip database $BDD"
+				unset BDD
+				break
+			fi								
+		done
 	
-					BDD=$(basename $i)
-					
-					for skip in "${DATABASES_SKIP[@]}"; do
-						if [ $BDD = $skip ]; then
-							f_log "Skip database $BDD"
-							unset BDD
-							break
-						fi								
-					done
-					
-					for select in "${DATABASES_SELECTED[@]}"; do
-						if [ $BDD != $select ]; then
-							f_log "Skip database $BDD"
-							unset BDD
-							break
-						fi								
-					done
+		if [ $BDD ]; then
+		
+			if [ -f $DIR/$BDD/__create.sql ]; then
+				f_log "Create database $BDD"
+				mysql --defaults-extra-file=$CONFIG_FILE < $DIR/$BDD/__create.sql 2>/dev/null
+			fi
+			
+			if [ $(database_exists $BDD) != "YES" ]; then
+				f_log "Error: Database $BDD dose not exists";
+			else
+			
+				tables=$(ls -1 $i |  grep -v __ | awk -F. '{print $1}' | sort | uniq)
+			
+				f_log "Create tables in $BDD"
+				for TABLE in $tables; do							
+					f_log "Create table: $BDD/$TABLE"
+					mysql --defaults-extra-file=$CONFIG_FILE $BDD -e "SET foreign_key_checks = 0;
+						DROP TABLE IF EXISTS $TABLE;
+						SOURCE $DIR/$BDD/$TABLE.sql;
+						SET foreign_key_checks = 1;"
+				done
 				
-					if [ $BDD ]; then
+				f_log "Import data into $BDD"		
+				for TABLE in $tables; do									
+					f_log "Import data into $BDD/$TABLE"
+						
+					if [ -f "$DIR/$BDD/$TABLE.txt.bz2" ]; then
+						f_log "< $TABLE"
+						if [ -f "$DIR/$BDD/$TABLE.txt" ]; then
+							rm $DIR/$BDD/$TABLE.txt
+						fi
+						bunzip2 $DIR/$BDD/$TABLE.txt.bz2
+					fi
 					
-						if [ -f $DIR/$BDD/__create.sql ]; then
-							f_log "Create database $BDD"
-							time mysql --defaults-extra-file=$CONFIG_FILE < $DIR/$BDD/__create.sql 2>/dev/null
+					if [ -f "$DIR/$BDD/$TABLE.txt" ]; then
+					
+						f_log "+ $TABLE"
+						split -l $CONFIG_CHUNK "$DIR/$BDD/$TABLE.txt" ${TABLE}_part_
+						for segment in ${TABLE}_part_*; do
+							time mysql --defaults-extra-file=$CONFIG_FILE $BDD --local-infile -e "SET foreign_key_checks = 0; SET unique_checks = 0; SET sql_log_bin = 0;
+								LOAD DATA LOCAL INFILE '$DIR/$BDD/$segment'
+								INTO TABLE $TABLE;
+								SET foreign_key_checks = 1; SET unique_checks = 1; SET sql_log_bin = 1;"
+							if [ -f "$DIR/$BDD/$segment" ]; then
+								rm $segment
+							fi
+						done																		
+						if [ ! -f "$DIR/$BDD/$TABLE.txt.bz2" ]; then
+							f_log "> $TABLE"
+							bzip2 $DIR/$BDD/$TABLE.txt
 						fi
 						
-						if [ $(database_exists $BDD) != "YES" ]; then
-							f_log "Error: Database $BDD dose not exists";
-						else
-						
-							tables=$(ls -1 $i |  grep -v __ | awk -F. '{print $1}' | sort | uniq)
-						
-							f_log "Create tables in $BDD"
-							for TABLE in $tables; do							
-							f_log "Create table: $BDD/$TABLE"
-							time mysql --defaults-extra-file=$CONFIG_FILE $BDD -e "SET foreign_key_checks = 0;
-													DROP TABLE IF EXISTS $TABLE;
-													SOURCE $DIR/$BDD/$TABLE.sql;
-													SET foreign_key_checks = 1;"
-							done
-							
-							f_log "Import data into $BDD"		
-							for TABLE in $tables; do									
-								f_log "Import data into $BDD/$TABLE"
-									
-								if [ -f "$DIR/$BDD/$TABLE.txt.bz2" ]; then
-									f_log "< $TABLE"
-									if [ -f "$DIR/$BDD/$TABLE.txt" ]; then
-										rm $DIR/$BDD/$TABLE.txt
-									fi
-									bunzip2 $DIR/$BDD/$TABLE.txt.bz2
-								fi
-								
-								if [ -f "$DIR/$BDD/$TABLE.txt" ]; then
-									f_log "+ $TABLE"
-									split -l $CONFIG_CHUNK "$DIR/$BDD/$TABLE.txt" ${TABLE}_part_
-									for segment in ${TABLE}_part_*; do
-										time mysql --defaults-extra-file=$CONFIG_FILE $BDD --local-infile -e "SET foreign_key_checks = 0; SET unique_checks = 0; SET sql_log_bin = 0;
-																		LOAD DATA LOCAL INFILE '$DIR/$BDD/$segment'
-																		INTO TABLE $TABLE;
-																		SET foreign_key_checks = 1; SET unique_checks = 1; SET sql_log_bin = 1;"
-										if [ -f "$DIR/$BDD/$segment" ]; then
-											rm $segment
-										fi
-									done																		
-									if [ ! -f "$DIR/$BDD/$TABLE.txt.bz2" ]; then
-										f_log "> $TABLE"
-										bzip2 $DIR/$BDD/$TABLE.txt
-									fi
-								fi
-								
-								if [ $DATABASES_TABLE_CHECK ]; then
-									if [ -f "$DIR/$BDD/$TABLE.ibd" ]; then
-										if [ ! $(innochecksum $DIR/$BDD/$TABLE.ibd) ]; then
-											f_log "$TABLE [OK]"
-										else
-											f_log "$TABLE [ERR]"
-										fi
-									fi
-								fi
-									
-							done						
-							
-							if [ -f "$DIR/$BDD/__routines.sql" ]; then
-									f_log "Import routines into $BDD"
-									time mysql --defaults-extra-file=$CONFIG_FILE $BDD < $DIR/$BDD/__routines.sql 2>/dev/null
+					fi
+					
+					if [ $DATABASES_TABLE_CHECK ]; then
+						if [ -f "$DIR/$BDD/$TABLE.ibd" ]; then
+							if [ ! $(innochecksum $DIR/$BDD/$TABLE.ibd) ]; then
+								f_log "$TABLE [OK]"
+							else
+								f_log "$TABLE [ERR]"
 							fi
-							
-							if [ -f "$DIR/$BDD/__views.sql" ]; then
-									f_log "Import views into $BDD"
-									time mysql --defaults-extra-file=$CONFIG_FILE $BDD < $DIR/$BDD/__views.sql 2>/dev/null
-							fi
-							
 						fi
-					fi				
+					fi
+						
+				done						
+				
+				if [ -f "$DIR/$BDD/__routines.sql" ]; then
+						f_log "Import routines into $BDD"
+						mysql --defaults-extra-file=$CONFIG_FILE $BDD < $DIR/$BDD/__routines.sql 2>/dev/null
+				fi
+				
+				if [ -f "$DIR/$BDD/__views.sql" ]; then
+						f_log "Import views into $BDD"
+						mysql --defaults-extra-file=$CONFIG_FILE $BDD < $DIR/$BDD/__views.sql 2>/dev/null
+				fi
+				
+			fi
+		fi				
 	done
 
 	f_log "Flush privileges;"
-	time mysql --defaults-extra-file=$CONFIG_FILE -e "flush privileges;"
+	mysql --defaults-extra-file=$CONFIG_FILE -e "flush privileges;"
 
 	f_log "** END **"
 }
