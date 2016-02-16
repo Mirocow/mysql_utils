14041fb59c200392dd95be1c3b499c15d860de82
mirocow <Mirocow@users.noreply.github.com>
2016-02-16 19:57:39 +0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -18,6 +18,13 @@ if [ ! -n "$BASH" ] ;then echo Please run this script $0 with bash; exit 1; fi
 # === FUNCTION ===
 f_log()
 {
+    local bold=$(tput bold)
+    local yellow=$(tput setf 6)
+    local red=$(tput setf 4)
+    local green=$(tput setf 2)
+    local reset=$(tput sgr0)
+    local toend=$(tput hpa $(tput cols))$(tput cub 6)	
+    
     logger "BACKUP: $@"
 
     if [ $VERBOSE -eq 1 ]; then
