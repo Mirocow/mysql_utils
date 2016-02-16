73fcf08477c90f57d363ecd11d917d27d9eb6a18
mirocow <Mirocow@users.noreply.github.com>
2016-02-16 19:56:49 +0400

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -21,6 +21,13 @@ database_exists()
 
 f_log() 
 {
+    local bold=$(tput bold)
+    local yellow=$(tput setf 6)
+    local red=$(tput setf 4)
+    local green=$(tput setf 2)
+    local reset=$(tput sgr0)
+    local toend=$(tput hpa $(tput cols))$(tput cub 6)	
+	
     logger "RESTORE: $@"
 	
     if [ $VERBOSE -eq 1 ]; then
