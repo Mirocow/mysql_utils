d327837acbd4cb3b22100246ba2bd269c8234dff
mirocow <mr.mirocow@gmail.com>
2023-01-25 18:55:44 +0300

    Added increase limits before restore data (fixed)

--- restore.sh
+++ restore.sh
@@ -119,7 +119,8 @@ restore()
                         fi
 
                         error=$(mysql --defaults-file=$CONFIG_FILE $DATABASE --local-infile -e "
-                        SET GLOBAL connect_timeout = 30;
+                        SET GLOBAL interactive_timeout=60;
+                        SET GLOBAL connect_timeout = 60;
                         SET GLOBAL net_buffer_length=1000000;
                         SET GLOBAL max_allowed_packet=1000000000;
                         SET SESSION sql_mode='NO_AUTO_VALUE_ON_ZERO';
